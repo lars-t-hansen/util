29d710d3b50db8075ca8cf816da8a26d46d2e81e
Lars T Hansen <larstha@uio.no>
2024-12-04 07:10:05 +0100

    Util

---
+++ csv2awk/csv2awk.go
@@ -0,0 +1,37 @@
+// Usage: csv2awk
+//
+// Reads stdin, writes to stdout
+
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	input := os.Stdin
+	output := bufio.NewWriter(os.Stdout)
+	defer output.Flush()
+
+	r := csv.NewReader(input)
+	r.FieldsPerRecord = -1
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, r := range records {
+		for i, f := range r {
+			if i > 0 {
+				fmt.Fprint(output, " ")
+			}
+			fmt.Fprint(output, strings.ReplaceAll(f, " ", "_"))
+		}
+		fmt.Fprintln(output, "")
+	}
+}
---
+++ csv2awk/go.mod
@@ -0,0 +1,3 @@
+module csv2awk
+
+go 1.23.3
