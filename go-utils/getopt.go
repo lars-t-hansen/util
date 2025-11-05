cae89b0c0d7c22bad15d544cf8e39872c60c88bf
Lars T Hansen <lars.t.hansen@gmail.com>
2025-11-05 22:33:23 +0100

    WIP

--- go-utils/getopt.go
+++ go-utils/getopt.go
@@ -1,24 +1,11 @@
 // SPDX-License-Identifier: MIT
 // From https://github.com/lars-t-hansen/util/go-utils
 
-// GNU-ish getopt-style option parser.
-
-package utils
-
-type Option[Cx any] struct {
-	Short rune
-	Long string
-	Help string
-	Value bool
-	Repeatable bool
-	Handler func(cx Cx, string) error
-}
-
-// GetOpts is a simple table-driven argument parser that is superficially compatible with GNU
+// GetOpts is a simple table-driven argument parser that is more or less compatible with GNU
 // getopt-style parsing.  The parser takes a table of options and handlers, a context argument to
-// pass to the handlers, a program name and a list of arguments, and parses the arguments, invoking
-// handlers for the various option names for each argument.  Argument parsing stops at '--', any
-// remaining arguments are returned as a slice from the call.
+// pass to the handlers, and a list of arguments, and parses the arguments, invoking handlers for
+// the various option names for each argument.  Argument parsing stops at '--', any remaining
+// arguments are returned as a slice from the call.
 //
 // An option can have a long and/or a short name.  If it has neither it is the default handler.
 //
@@ -36,21 +23,38 @@ type Option[Cx any] struct {
 // Short names be run together with a value for at most one of the options, as in '-nkr2' for
 // 'sort(1)', equivalent to -n -k 2 -r; also '-nkr 2' is accepted.  Again the boundary between
 // option letters and value in the former case is where a letter is not a valid option letter, so
-// '-nk2r' won't work.
+// '-nk2r' won't work as this is interpreted as '-n -k 2r'.
 //
 // Failure to validate the option table results in a panic.  Any error returned from GetOpts is an
 // error returned from the parser due to an unknown option or from one of the handlers as a
-// validation error, the latter wrapped in a parser error explaining the context.
+// validation error, the latter usually wrapped in a parser error explaining the context.
 //
 // Other than the default option, an option is not repeatable unless its Repeatable attribute is
-// set.
-//
-// The help text is documentation that is not used by GetOpts, but locating it in the options table
-// may be useful for other parts of the program.
-func GetOpts[Cx any](options []Option[Cx], cx Cx, progname string, args []string) (rest []string, parseErr error) {
+// set.  (TODO: That is broken.  There's no reason to assume the default option should always be
+// repeatable, and no way to turn it off short of adding logic in the handler.)
+package utils
+
+import (
+	"fmt"
+	"io"
+	"strings"
+)
+
+type Option[Cx any] struct {
+	Short      rune
+	Long       string
+	Help       string
+	Value      bool
+	Repeatable bool
+	Handler    func(cx Cx, value string) error
+}
+
+// Parse args, passing argument values to the handlers of options along with the cx, and return any
+// left-over arguments.
+func GetOpts[Cx any](options []Option[Cx], cx Cx, args []string) (rest []string, parseErr error) {
 	// These maps map the option name or character without the leading '-' or '--'.
-	short := make(map[rune]*Option)
-	long := make(map[string]*Option)
+	short := make(map[rune]*Option[Cx])
+	long := make(map[string]*Option[Cx])
 	handled := make(map[any]bool)
 	var defaultHandler func(Cx, string) error
 
@@ -58,17 +62,17 @@ func GetOpts[Cx any](options []Option[Cx], cx Cx, progname string, args []string
 	for i := range options {
 		o := &options[i]
 		if o.Handler == nil {
-			panic("Option without handler at loc %d: short=%c long=%s", i, o.Short, o.Long)
+			panic(fmt.Sprintf("Option without handler at loc %d: short=%c long=%s", i, o.Short, o.Long))
 		}
 		if o.Short == 0 && o.Long == "" {
 			if defaultHandler != nil {
 				panic("Multiple default handlers")
 			}
-			defaultHandler = o
+			defaultHandler = o.Handler
 		}
 		if o.Short != 0 {
 			if short[o.Short] != nil {
-				panic("Multiple definitions for short option %c", o.Short)
+				panic(fmt.Sprintf("Multiple definitions for short option %c", o.Short))
 			}
 			if o.Short == '-' {
 				if o.Long != "" {
@@ -78,11 +82,11 @@ func GetOpts[Cx any](options []Option[Cx], cx Cx, progname string, args []string
 					panic("Lone dash cannot take a value")
 				}
 			}
-			short[i.Short] = o
+			short[o.Short] = o
 		}
 		if o.Long != "" {
 			if long[o.Long] != nil {
-				panic("Multiple definitions for long option %s", o.Long)
+				panic(fmt.Sprintf("Multiple definitions for long option %s", o.Long))
 			}
 			long[o.Long] = o
 		}
@@ -108,29 +112,29 @@ func GetOpts[Cx any](options []Option[Cx], cx Cx, progname string, args []string
 				optname, value, matched := strings.Cut(arg, "=")
 				opt := long[optname]
 				if opt == nil {
-					parseErr = fmt.Errorf("Unknown option \"%s\"", optname)
+					parseErr = fmt.Errorf("Unknown option \"--%s\"", optname)
 					return
 				}
 				if !opt.Repeatable && handled[optname] {
-					parseErr = fmt.Errorf("Repeated but unrepeatable option \"%s\"", optname)
+					parseErr = fmt.Errorf("Repeated but unrepeatable option \"--%s\"", optname)
 					return
 				}
 				handled[optname] = true
 				if !opt.Value && matched {
-					parseErr = fmt.Errorf("Option \"%s\" does not take a value", optname)
+					parseErr = fmt.Errorf("Option \"--%s\" does not take a value", optname)
 					return
 				}
 				if opt.Value && !matched {
 					if argIx == len(args) {
-						parseErr = fmt.Errorf("Missing value for option \"%s\"", optname)
+						parseErr = fmt.Errorf("Missing value for option \"--%s\"", optname)
 						return
 					}
 					value = args[argIx]
 					argIx++
 				}
-				parseErr = opt.Handler(value)
+				parseErr = opt.Handler(cx, value)
 				if parseErr != nil {
-					parseErr = fmt.Errorf("Rejected option \"%s\": %w", r)
+					parseErr = fmt.Errorf("Rejected option \"--%s\": %w", optname, parseErr)
 					return
 				}
 			} else {
@@ -139,11 +143,12 @@ func GetOpts[Cx any](options []Option[Cx], cx Cx, progname string, args []string
 					i++
 				}
 				var needValue func(Cx, string) error
-				loop {
+				var optThatNeedsIt rune
+				for {
 					if i == len(arg) {
 						break
 					}
-					optname := arg[i]
+					optname := rune(arg[i])
 					opt := short[optname]
 					i++
 					if opt == nil {
@@ -154,7 +159,7 @@ func GetOpts[Cx any](options []Option[Cx], cx Cx, progname string, args []string
 						break
 					}
 					if !opt.Repeatable && handled[optname] {
-						parseErr = fmt.Errorf("Repeated but unrepeatable option \"%c\"", optname)
+						parseErr = fmt.Errorf("Repeated but unrepeatable option \"-%c\"", optname)
 						return
 					}
 					handled[optname] = true
@@ -164,6 +169,7 @@ func GetOpts[Cx any](options []Option[Cx], cx Cx, progname string, args []string
 							return
 						}
 						needValue = opt.Handler
+						optThatNeedsIt = optname
 					} else {
 						parseErr = opt.Handler(cx, "")
 						if parseErr != nil {
@@ -177,7 +183,7 @@ func GetOpts[Cx any](options []Option[Cx], cx Cx, progname string, args []string
 						value = arg[i:]
 					} else {
 						if argIx == len(args) {
-							parseErr = fmt.Errorf("Missing value for option \"%s\"", optname)
+							parseErr = fmt.Errorf("Missing value for option \"-%c\"", optThatNeedsIt)
 							return
 						}
 						value = args[argIx]
@@ -190,7 +196,7 @@ func GetOpts[Cx any](options []Option[Cx], cx Cx, progname string, args []string
 				}
 			}
 		} else {
-			parseErr = defaultHandler(a)
+			parseErr = defaultHandler(cx, arg)
 			if parseErr != nil {
 				return
 			}
@@ -199,11 +205,14 @@ func GetOpts[Cx any](options []Option[Cx], cx Cx, progname string, args []string
 	return
 }
 
-func PrintOptions[Cx any](output io.Writer, options []Option[Cx]) {
+// PrintOpts prints the option names and help text of the options table in a sensible way on output,
+// as for a usage message.
+func PrintOpts[Cx any](output io.Writer, options []Option[Cx]) {
 	// Print a line with short and long options
 	// Print help indented on next line
 	// Don't be fancy
 	for _, o := range options {
+		_ = o
 	}
 }
 
--- go-utils/getopt_test.go
+++ go-utils/getopt_test.go
@@ -1,63 +1,81 @@
 package utils
 
+import (
+	"fmt"
+	"os"
+)
+
+func pushInputFile(_ bool, n string) error {
+	return nil
+}
+
+var (
+	namesFromStdin     bool
+	printHelp          bool
+	printVersion       bool
+	verbose            bool
+	outname            string
+	systemEtagsCommand string
+)
+
 func example() {
 	opts := []Option[bool]{
-		Option{
-			Short: 'h',
-			Long: "help",
-			Help: "Print help",
-			Handler: SetFlag(&printHelp),
+		Option[bool]{
+			Short:   'h',
+			Long:    "help",
+			Help:    "Print help",
+			Handler: SetFlag[bool](&printHelp),
 		},
-		Option{
-			Short: 'o',
-			Help: fmt.Sprintf(`Filename of output file, "-" for stdout, default "%s"`, outname),
-			Value: true,
-			Handler: SetString(&outname)
+		Option[bool]{
+			Short:   'o',
+			Help:    fmt.Sprintf(`Filename of output file, "-" for stdout, default "%s"`, outname),
+			Value:   true,
+			Handler: SetString[bool](&outname),
 		},
-		Option{
-			Short: 'v',
-			Help: "Enable verbose output (for debugging)",
-			Handler: SetFlag(&verbose),
+		Option[bool]{
+			Short:   'v',
+			Help:    "Enable verbose output (for debugging)",
+			Handler: SetFlag[bool](&verbose),
 		},
-		Option{
-			Short: 'V',
-			Long: "version",
-			Help: "Print version information",
-			Handler: SetFlag(&printVersion),
+		Option[bool]{
+			Short:   'V',
+			Long:    "version",
+			Help:    "Print version information",
+			Handler: SetFlag[bool](&printVersion),
 		},
-		Option{
-			Long: 'etags',
+		Option[bool]{
+			Long:  "etags",
 			Value: true,
 			Help: fmt.Sprintf(
 				`Path of the native etags program, "" to disable this functionality, default "%s"`,
 				systemEtagsCommand,
 			),
-			Handler: SetString(&systemEtagsCommand),
+			Handler: SetString[bool](&systemEtagsCommand),
 		},
-		Option{
-			Short: '-',
-			Handler: SetFlag(&namesFromStdin),
+		Option[bool]{
+			Short:   '-',
+			Handler: SetFlag[bool](&namesFromStdin),
 		},
-		Option{
+		Option[bool]{
 			// Implicitly repeatable
 			Handler: pushInputFile,
 		},
 	}
-	if rest, err := GetOpts(opts, true, os.Args[0], os.Args[1:]); err != nil {
-		...;
+	rest, err := GetOpts(opts, true, os.Args[1:])
+	if err != nil {
+		panic(err)
 		os.Exit(2)
 	}
+	_ = rest
 	if printHelp {
-		...;
+		// ...;
 		os.Exit(0)
 	}
 	if printVersion {
-		...;
+		//...;
 		os.Exit(0)
 	}
 	for _, r := range rest {
-		pushInputFile(r)
+		pushInputFile(true, r)
 	}
-
 }
-
